a57af25d3ae6f8d9fcb8873baf2a856cb51be2f4
RISHABH SETH <coding70@users.noreply.github.com>
2021-12-22 23:26:07 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -85,10 +85,10 @@ var personSchema = new mongoose.Schema({
   name: String,
   age: Number,
   favoriteFoods: [String]
-});
+});*/
 
 /** 3) Create and Save a Person */
-var Person = mongoose.model('Person', personSchema,'person');
+/*var Person = mongoose.model('Person', personSchema,'person');
 
 var createAndSavePersoon = function(done) {
   var janeFonda = new Person({name: personName, age: personAge, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
