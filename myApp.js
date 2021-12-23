b92af2b9db58ae7c14e225e2f7d066fafc0d9ec3
RISHABH SETH <coding70@users.noreply.github.com>
2021-12-23 13:16:20 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -88,10 +88,12 @@ const removeById = (personId, done) => {
 };
 
 const removeManyPeople = (done) => {
-    const nameToRemove = "Mary";
-  Person.deleteMany({name: nameToRemove}, (err, response) => {
-    if(err) return console.log(err);
-    done(null, response);
+  const nameToRemove = "Mary";
+  Person.remove({name:nameToRemove},(err,data)=>{
+    if(err) return console.error(err);
+    data.ok = true;
+    data.n = data.deletedCount;
+    done(null , data);
   });
 };
 
