eacedc5cd5aa8fc06be8b33c82245de3e54c7838
RISHABH SETH <coding70@users.noreply.github.com>
2021-12-23 10:47:39 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -97,13 +97,12 @@ const removeManyPeople = (done) => {
 };
 
 const queryChain = (done) => {
-const foodToSearch = "burrito";
-  Person.find({ age: 55 })
-  .sort({ name: -1 })
-  .limit(5)
-  .select({ favoriteFoods: 0 })
+ const foodToSearch = "burrito";
+  Person.find({favoriteFoods: foodToSearch })
+  .sort({ name: 1 })
+  .limit(2)
+  .select({ age: 0 })
   .exec(function(error, people) {
-    //do something here
     if(err) return console.log(err);
     done(null , people);
   });
